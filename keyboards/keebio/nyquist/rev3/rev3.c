e5a417ab5b8f9319d105fb2f470eb86be1965b02
Kyle San Clemente <ksanclemente@acorns.com>
2021-08-08 22:23:43 -0700

    Fix qmk build

--- keyboards/keebio/nyquist/rev3/rev3.c
+++ keyboards/keebio/nyquist/rev3/rev3.c
@@ -13,17 +13,6 @@ void eeconfig_init_kb(void) {
 #endif
 #endif
 
-//void matrix_init_kb(void) {
-//
-//     // green led on
-//     DDRD |= (1<<5);
-//     PORTD &= ~(1<<5);
-//
-//    // // orange led on
-//    // DDRB |= (1<<0);
-//    // PORTB &= ~(1<<0);
-//
-//	matrix_init_user();
-//};
     eeconfig_update_kb(0);
     eeconfig_init_user();
+}
